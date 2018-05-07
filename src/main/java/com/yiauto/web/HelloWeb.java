e91532f5271366716df0645a57c5287cef2dfb5f
zhiheng.li <1249913118@qq.com>
2018-05-07 18:27:48 +0800

    初始化项目

---
+++ src/main/java/com/yiauto/web/HelloWeb.java
@@ -0,0 +1,21 @@
+package com.yiauto.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create date 2018/5/7
+ * <p>描述</p>
+ *
+ * @author zhiheng.li
+ * @since 1.0.
+ */
+@SpringBootApplication
+public class HelloWeb {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWeb.class, args);
+    }
+}
